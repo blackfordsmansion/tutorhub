2df35aee690f240f037c6decac2738340a69d96e
blackfordsmansion <lamborghini4life123@gmail.com>
2025-08-09 14:29:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+﻿function setupCurriculumSlider(curriculum) {
+  const slider = document.getElementById('gradeSlider');
+  const label = document.getElementById('gradeLabel');
+  const curriculumDisplay = document.getElementById('curriculumText');
+
+
+  // Change max value of slider to reflect K-8
+  slider.max = 8;
+
+
+  function updateCurriculum() {
+    const grade = parseInt(slider.value, 10);
+    let displayGrade = grade === 0 ? 'Kindergarten' : `Grade ${grade}`;
+    label.textContent = displayGrade;
+    curriculumDisplay.textContent = curriculum[grade];
+  }
+
+
+  slider.addEventListener('input', updateCurriculum);
+
+
+  // Initialize display
+  updateCurriculum();
+}
