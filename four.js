7bd5bf31f3d08d1ffcbca81b240d652478056e05
blackfordsmansion <lamborghini4life123@gmail.com>
2025-08-12 17:05:50 -0400

    Delete four.js

--- four.js
+++
@@ -1,9 +0,0 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("tutoring-form");
-
-
-    form.addEventListener("submit", function (e) {
-        // Optionally show a loading message before submit
-        console.log("Form submitted");
-    });
-});
